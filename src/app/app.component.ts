56139849994f89eb6a9d86b7ef65e3d98e7961df
Sairam-Reddy <sairamreddyg33@gmail.com>
2022-02-21 17:03:57 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  public counter: number = 10000000000000;
+  public counter: number = 1000;
 
   public increment(): void {
     this.counter = this.counter + 100;
